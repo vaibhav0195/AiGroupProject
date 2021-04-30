c678935a6e66c91235051d18112d69353c35fd3d
Vardan Kaushik <59229457+VardanKaushik@users.noreply.github.com>
2021-05-01 00:02:18 +0100

    Add files via upload

--- Minesweeper.py
+++ Minesweeper.py
@@ -17,9 +17,9 @@ class Minesweeper:
         self.no_mines = no_mines
         self.gridsize = gridsize
 
-        self.nodes = [[Node((i, j), False) for j in range(gridsize)] for i in range(gridsize)]
+        self.nodes = [[Node((i,j), False) for j in range(gridsize)] for i in range(gridsize)]
 
-        mine_indices = random.sample([(a, b) for a in range(gridsize) for b in range(gridsize)], k=no_mines)
+        mine_indices = random.sample([(a,b) for a in range(gridsize) for b in range(gridsize)], k=no_mines)
         for mine_node_loc in mine_indices:
             x, y = mine_node_loc[0], mine_node_loc[1]
             self.nodes[x][y].is_mine = True
@@ -30,13 +30,6 @@ class Minesweeper:
                     self.nodes[x + i][y + j].adjacent_mines += 1
 
         assert sum(n.is_mine for n in self.get_flat_nodes()) == no_mines
-
-    def __str__(self):
-        s = ""
-        for r in self.nodes:
-            for n in r:
-                s += str(n) + '\n'
-        return s
 
     def get_flat_nodes(self):
         return [n for r in self.nodes for n in r]
@@ -52,8 +45,8 @@ class Minesweeper:
 
     def click(self, x, y):
         if self.nodes[x][y].is_mine:
-            print(f"GAME OVER: Clicked on" + str(self.nodes[x][y]))
-            exit()
+            print(f"GAME OVER: Clicked on a mine " + str(self.nodes[x][y]))
+            return 0
 
         self.nodes[x][y].is_unrevealed = False
 
@@ -65,14 +58,15 @@ class Minesweeper:
             for j in range(-1, 2):
                 if not (0 <= x + i < self.gridsize and 0 <= y + j < self.gridsize):
                     continue
-                if self.nodes[x + i][y + j].adjacent_mines == 0 and self.nodes[x + i][y + j].is_unrevealed:
-                    self.nodes[x + i][y + j].is_unrevealed = False
-                    self.open_empty_space_recursive(x + i, j + y)
+                if self.nodes[x+i][y+j].adjacent_mines == 0 and self.nodes[x+i][y+j].is_unrevealed:
+                    self.nodes[x+i][y+j].is_unrevealed = False
+                    self.open_empty_space_recursive(x+i, j+y)
                 else:
-                    self.nodes[x + i][y + j].is_unrevealed = False
+                    self.nodes[x+i][y+j].is_unrevealed = False
 
     def start_game(self):
-        starting_node = random.choice([n for n in self.get_flat_nodes() if n.adjacent_mines == 0])
+        #starting_node = random.choice([n for n in self.get_flat_nodes() if n.adjacent_mines == 0])
+        starting_node = random.choice([n for n in self.get_flat_nodes() ])
         self.click(starting_node.pos[0], starting_node.pos[1])
 
     def get_msnode_from_ainode(self, n):
